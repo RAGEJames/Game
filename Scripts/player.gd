a0895b1cfffc98429601bbbdccdff05be9c3ebc1
RAGEJames <jamesbondin5000@gmail.com>
2025-07-13 02:06:49 -0400

    autocommit

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -34,6 +34,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("W") and is_on_floor():
 		velocity.y = JUMP_VELOCITY - JUMP_REDUCE
 		JUMP_REDUCE = JUMP_REDUCE - 120.0
+		get_node("/root/World/W1/PLAYER/Health").hp50()
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY - JUMP_REDUCE
 		JUMP_REDUCE = JUMP_REDUCE - 120.0
